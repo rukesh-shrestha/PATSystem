b173820c703fd5ff11339ea35dcc5bdd47463a24
RukeshShrestha <rukesh.shrestha11@gmail.com>
2023-08-05 14:51:35 +0545

    Account verification of custom account done. Template Updated.

--- utils/sendVerificationMail.js
+++ utils/sendVerificationMail.js
@@ -5,15 +5,37 @@ const sendVerificationEmail = (user) => {
   const mailOptions = {
     from: '"Rukesh Shrestha" <rukeshportfolio@gmail.com>',
     to: user.email,
-    subject: "Email Verification",
+    subject: "Verify Your Account - Action Required",
     html: `
-    <p>Hello ${user.name}</p>,
+    <p>Hello ${user.firstName},</p>
 
     <div>
-        In order to continue with the system. You can verify your account by clicking the button below. 
-        <hr>
+        Thank you for signing up with our platform! We're excited to have you on board. To ensure the security of your account and complete the registration process, we kindly ask you to verify your account by clicking the link below:        
 
-        <a href='${process.env.DOMAIN_NAME}/api/users/verify-email?emailToken=${user.emailToken}'>Verify Your Email</a>
+        <br>
+        <br>
+        
+        
+        <a href='${process.env.DOMAIN_NAME}/api/users/verify-email?emailToken=${user.emailToken}' style="text-decoration:none">Verify Your Email</a>
+        
+        <br>
+        <br>
+
+        Once verified, you'll have full access to all the features and services we offer.If you did not sign up for an account on our platform, please disregard this email. Your account will not be activated until you verify it using the provided link.
+        <br>    
+
+        If you encounter any issues or have any questions, feel free to contact at rukesh.shrestha11@gmail.com or rukesh.shrestha@heraldcollege.edu.np. We're here to help!
+        <br>        
+
+        Thank you for using the system.
+        <br>
+        <br>      
+
+        
+        Best Regards,<br>
+        Rukesh Shrestha
+        
+        
     </div>
     `,
   };
